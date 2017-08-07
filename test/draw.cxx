977a2ae4b74ee21db7822fdb937c98b3993430c6
amassiro <massironi.andrea@gmail.com>
2017-08-07 16:16:03 +0200

    draw

---
+++ test/draw.cxx
@@ -0,0 +1,132 @@
+
+
+
+TH1F * DrawOverflow(TH1F *h) {
+  // This function paint the histogram h with an extra bin for overflows
+  UInt_t nx = h->GetNbinsX()+1;
+  Double_t *xbins= new Double_t[nx+1];
+  for (UInt_t i=0;i<nx;i++)
+    xbins[i]=h->GetBinLowEdge(i+1);
+  xbins[nx]=xbins[nx-1]+h->GetBinWidth(nx);
+  char *tempName= new char[strlen(h->GetName())+10];
+  sprintf(tempName,"%swtOverFlow",h->GetName());
+  // Book a temporary histogram having ab extra bin for overflows
+  TH1F *htmp = new TH1F(tempName, h->GetTitle(), nx, xbins);
+  //---- style
+  htmp->SetLineColor(h->GetLineColor());
+  htmp->SetLineWidth(h->GetLineWidth());
+  htmp->SetLineStyle(h->GetLineStyle());
+  // Reset the axis labels
+  htmp->SetXTitle(h->GetXaxis()->GetTitle());
+  htmp->SetYTitle(h->GetYaxis()->GetTitle());
+  // Fill the new hitogram including the extra bin for overflows
+  for (UInt_t i=1; i<=nx; i++)
+    htmp->Fill(htmp->GetBinCenter(i), h->GetBinContent(i));
+  // Fill the underflows
+  htmp->Fill(h->GetBinLowEdge(1)-1, h->GetBinContent(0));
+  // Restore the number of entries
+  htmp->SetEntries(h->GetEntries());
+  // FillStyle and color
+  htmp->SetFillStyle(h->GetFillStyle());
+  htmp->SetFillColor(h->GetFillColor());
+  return htmp;
+}
+
+#include "TDRStyle.cc"
+
+void draw(std::string nameInFileRootOne, 
+          std::string nameInFileRootTwo, 
+          std::string var = "etaSC", int NBIN = 1000, float MIN = 0, float MAX = 1000, std::string varHR = ""
+          ) {
+  
+   
+  TDRStyle();
+  gStyle->SetTitleYOffset(1.1);
+  gStyle->cd();
+  
+  if (varHR == "") {
+    varHR = var;
+  }
+  gStyle->SetOptStat(0);
+  
+  
+  TFile* f_1;
+  TFile* f_2;
+  
+  f_1 = new TFile (nameInFileRootOne.c_str());
+  f_2 = new TFile (nameInFileRootTwo.c_str());
+
+  TTree* t_1;
+  TTree* t_2;
+  
+  t_1 = (TTree*) f_1 -> Get ("tree");
+  t_2 = (TTree*) f_2 -> Get ("tree");
+          
+  
+  TH1F* h_1;
+  TH1F* h_2;
+  
+  h_1 = new TH1F("h_1","before",NBIN,MIN,MAX);
+  h_2 = new TH1F("h_2","after",NBIN,MIN,MAX);
+     
+  h_1->Sumw2();
+  h_2->Sumw2();
+  
+  TString weight = Form ("evtWeight");
+//   TString weight = Form ("1");
+  TString toDraw;
+    
+  toDraw = Form ("%s >> h_1",var.c_str());
+  t_1->Draw(toDraw.Data(),weight.Data(),"goff");
+
+  toDraw = Form ("%s >> h_2",var.c_str());
+  t_2->Draw(toDraw.Data(),weight.Data(),"goff");
+  
+  h_1->SetMarkerSize  (0);
+  h_1->SetMarkerColor (kBlue);
+  h_1->SetLineColor   (kBlue);
+  
+  h_2->SetMarkerSize  (0);
+  h_2->SetMarkerColor (kRed);
+  h_2->SetLineColor   (kRed);
+
+  h_1->SetLineWidth(2);
+  h_2->SetLineWidth(2);
+  
+  h_1->SetLineStyle(2);
+  h_2->SetLineStyle(3);
+  
+  h_1->GetXaxis()->SetTitle(varHR.c_str());
+  
+  
+  TLegend* leg = new TLegend(0.80,0.70,0.95,0.90);
+  leg->AddEntry(h_1, h_1->GetTitle(), "l");
+  leg->AddEntry(h_2, h_2->GetTitle(), "l");
+  leg->SetFillStyle(0);
+
+  //---- overflow bin
+  h_1 = DrawOverflow(h_1);
+  h_2 = DrawOverflow(h_2);
+  
+  //---- draw
+  TCanvas* cn = new TCanvas ("cn","cn",800,600);
+  
+  h_1->Draw("hist");
+  h_2->Draw("hist same");
+  
+  leg->Draw();
+  cn->SetGrid();
+
+  //---- draw
+  TCanvas* cnNorm = new TCanvas ("cnNorm","cnNorm",800,600);
+  
+  h_1->DrawNormalized("hist");
+  h_2->DrawNormalized("hist same");
+  
+  leg->Draw();
+  cnNorm->SetGrid();
+  
+  
+}
+
+
